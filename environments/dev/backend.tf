176d4fc8b769384004d7625ca6c7f038ae14111e
Raj Kishore Jha <rajisha0202@gmail.com>
2026-01-18 14:48:19 +0530

    Configure Terraform backend for development environment

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstateplatform"
+    container_name       = "tfstate"
+    key                  = "dev/platform.tfstate"
+  }
+}
 
