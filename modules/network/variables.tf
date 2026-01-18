4b5ebbc3ccb4a0247183d07153701da9c7540c3a
Raj Kishore Jha <rajisha0202@gmail.com>
2026-01-18 14:51:40 +0530

    Define network module variables in variables.tf
    Added variables for resource group, location, VNet name, address space, and subnet prefixes.

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,1 +1,25 @@
+variable "resource_group_name" {
+  description = "Resource group where network will be created"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Virtual network name"
+  type        = string
+}
+
+variable "address_space" {
+  description = "VNet address space"
+  type        = list(string)
+}
+
+variable "subnet_prefixes" {
+  description = "Map of subnet names to CIDR blocks"
+  type        = map(string)
+}
 
