b5d911868829bacd978419c9a26931079086a0b3
Raj Kishore Jha <rajisha0202@gmail.com>
2026-01-18 14:49:42 +0530

    Add variables for resource group and location

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,1 +1,10 @@
+variable "resource_group_name" {
+  description = "Resource group for platform"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
 
